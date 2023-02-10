cb514165046c0e8945b64091139e9d74aaa577ac
mahteuso <mateus@ufscar.br>
2023-02-10 01:46:21 -0500

    First function

--- count_caracter_simple.py
+++ count_caracter_simple.py
@@ -9,20 +9,7 @@ def count_character(s):
     print(dic_letters)
     
     
-    """Using list
-    ordened_words = sorted(s)
-    previus_letter = ordened_words[0]
-    count = 1
 
-    for letter in ordened_words[1:]:
-        if letter == previus_letter:
-            count += 1
-        else:
-            print(f"{previus_letter}: {count}")
-            previus_letter = letter
-            count = 1
-    print(f"{letter}: {count}")
-    """
 
 if __name__ == '__main__':
     count_character('banana')
