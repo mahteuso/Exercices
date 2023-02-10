45e58dcb584292e43c13fb1b777b331b272d652b
mahteuso <mateus@ufscar.br>
2023-02-10 06:50:51 -0500

    First function

--- test_Oo1.py
+++ test_Oo1.py
@@ -1,10 +1,13 @@
 def create_account(number, user, balance, limit):
     account_1 = {"number": number, "user": user, "balance": balance, "limit": limit}
     return account_1
+
 def extract(account):
     print(f"Your balance is: {account['balance']}")
+
 def deposit(account, value):
     account["balance"] += value
+
 def cash(account, value):
     if account["balance"] <= 0:
         account["limit"] = account["limit"] - value
