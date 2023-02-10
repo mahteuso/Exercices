01926135a9a11d45d5d7911d50963d8813fc9a3a
mahteuso <118922523+mahteuso@users.noreply.github.com>
2023-02-10 09:46:24 -0500

    Update count_caracter_simple.py

--- count_caracter_simple.py
+++ count_caracter_simple.py
@@ -8,6 +8,20 @@ def count_character(s):
         dic_letters[letter] = dic_letters.get(letter, 0) + 1
     print(dic_letters)
     
+    """Using list
+    ordened_words = sorted(s)
+    previus_letter = ordened_words[0]
+    count = 1
+
+    for letter in ordened_words[1:]:
+        if letter == previus_letter:
+            count += 1
+        else:
+            print(f"{previus_letter}: {count}")
+            previus_letter = letter
+            count = 1
+    print(f"{letter}: {count}")
+    """
     
 
 
