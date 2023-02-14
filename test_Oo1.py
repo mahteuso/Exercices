6aa4806207b4c8a76f5d110224ee499ff5e7449f
mahteuso <mateus@ufscar.br>
2023-02-14 00:17:59 -0500

    implement functions

--- test_Oo1.py
+++ test_Oo1.py
@@ -1,16 +1,24 @@
+"""
+Learning about object orientation
+"""
+
+
 def create_account(number, user, balance, limit):
     account_1 = {"number": number, "user": user, "balance": balance, "limit": limit}
     return account_1
 
+
 def extract(account):
     print(f"Your balance is: {account['balance']}")
+
 
 def deposit(account, value):
     account["balance"] += value
 
+
 def cash(account, value):
-    if account["balance"] <= 0:
+    if account["balance"] < value:
         account["limit"] = account["limit"] - value
-        account["balance"] = account["limit"]
+        account["balance"] -= account["limit"]
     else:
         account["balance"] -= value
